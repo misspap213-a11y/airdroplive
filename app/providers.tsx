29bcc5f2ae39ac41fd993355d6a968850316c1c1
misspap213-a11y <misspap213@gmail.com>
2025-11-03 05:06:37 +0100

    Update providers.tsx

--- app/providers.tsx
+++ app/providers.tsx
@@ -1,28 +1,33 @@
 "use client";
-import '@rainbow-me/rainbowkit/styles.css';
-import { getDefaultWallets, RainbowKitProvider } from '@rainbow-me/rainbowkit';
-import { configureChains, createConfig, WagmiConfig } from 'wagmi';
-import { mainnet } from 'wagmi/chains';
-import { publicProvider } from 'wagmi/providers/public';
-import { createPublicClient, http } from 'viem';
+import "@rainbow-me/rainbowkit/styles.css";
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+import { WagmiConfig, createConfig, configureChains } from "wagmi";
+import { mainnet } from "wagmi/chains";
+import { createPublicClient, http } from "viem";
 
-const { chains, publicClient } = configureChains([mainnet], [publicProvider()]);
+const { chains } = configureChains(
+  [mainnet],
+  [() => createPublicClient({ chain: mainnet, transport: http() })]
+);
+
 const { connectors } = getDefaultWallets({
-  appName: 'ARCLIVE Airdrop',
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || 'demo',
-  chains
+  appName: "ARCLIVE Airdrop",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "demo",
+  chains,
 });
 
-const wagmiConfig = createConfig({
+const config = createConfig({
   autoConnect: true,
   connectors,
-  publicClient: createPublicClient({ transport: http() })
+  publicClient: createPublicClient({ chain: mainnet, transport: http() }),
 });
 
 export default function Providers({ children }) {
   return (
-    <WagmiConfig config={wagmiConfig}>
-      <RainbowKitProvider chains={chains} coolMode>{children}</RainbowKitProvider>
+    <WagmiConfig config={config}>
+      <RainbowKitProvider chains={chains} coolMode>
+        {children}
+      </RainbowKitProvider>
     </WagmiConfig>
   );
 }
